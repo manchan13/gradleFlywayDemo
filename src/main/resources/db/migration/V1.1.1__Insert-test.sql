dad11a42e0aeedbdb76a9f25de70bbc61e728cf9
manchan13 <13manchan13@gmail.com>
2020-06-09 00:03:33 +0900

    Initial Commit

---
+++ src/main/resources/db/migration/V1.1.1__Insert-test.sql
@@ -0,0 +1,2 @@
+INSERT INTO message(id, title, message) VALUES ('6b23257c-e9d9-4d1e-ba79-01f8b8715ba9', 'INIT', 'Inserted by FLYWAY');
+INSERT INTO message(id, title, message) VALUES ('52345678-e9d9-4d1e-ba79-01f8b8715ba9', 'INIT', 'Inserted by FLYWAY');
